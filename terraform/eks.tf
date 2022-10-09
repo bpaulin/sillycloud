f2e0aba110be2d3e778e1d7f1ef5f0b9658d32ca
Bruno Paulin <brunopaulin@bpaulin.net>
2022-10-09 06:39:50 +0200

    eks

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,66 +1,14 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
+resource "aws_eks_cluster" "sillykloud" {
+  name = "sillykloud"
 
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-}
-
-resource "aws_eks_cluster" "example" {
-  name     = "example"
-  role_arn = aws_iam_role.example.arn
+  # Iam
+  role_arn = aws_iam_role.eks-sillykloud.arn
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-sillykloud-AmazonEKSClusterPolicy,
+  ]
 
   vpc_config {
     subnet_ids = [aws_subnet.main.id]
   }
-
-    # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-    # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-    depends_on = [
-      aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
-      aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
-    ]
 }
 
-output "endpoint" {
-  value = aws_eks_cluster.example.endpoint
-}
-
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.example.certificate_authority[0].data
-}
-
-
-###
-
-resource "aws_iam_role" "example" {
-  name = "eks-cluster-example"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.example.name
-}
-
-# Optionally, enable Security Groups for Pods
-# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.example.name
-}
